af9286197ba6316b2a883d447128e007287fbd8e
Peter Chow <peteroid.howinai@gmail.com>
2021-04-04 18:19:49 +0900

    support i18n

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import '../utilities/i18n';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import { useTranslation } from 'react-i18next';
+
 export default function Home() {
-  return <div>Home</div>;
+  const { t } = useTranslation();
+  return <div>{`Home, ${t('title')}`}</div>;
 }
