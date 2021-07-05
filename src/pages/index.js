f390d5e658d9c7dd76775bd837006ef07b613c19
Peter Chow <selllfocard@gmail.com>
2021-07-05 12:46:22 +0900

    add i18n link example

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,14 @@ import { useTranslation } from 'react-i18next';
 
 export default function Home() {
   const { t } = useTranslation();
-  return <div>{`Home, ${t('title')}`}</div>;
+  return (
+    <div>
+      <div>{`Home, ${t('title')}`}</div>
+      <div>
+        <a href='/?lng=zh'>View zh-lang</a>
+        <br />
+        <a href='/?lng=en'>View en-lang</a>
+      </div>
+    </div>
+  );
 }
