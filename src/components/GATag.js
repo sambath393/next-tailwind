078d46e0bea968b6af749e9996dad1d930e03e87
Peter Chow <selllfocard@gmail.com>
2021-09-10 21:16:45 +0900

    add GA and FB

---
+++ src/components/GATag.js
@@ -0,0 +1,24 @@
+const GATag = () => {
+  const GAId = process.env.NEXT_PUBLIC_GA_ID;
+  if (!GAId) return null;
+
+  const GACode = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '${GAId}');
+  `;
+  return (
+    <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${GAId}`} />
+      <script
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: GACode }}
+      />
+    </>
+  );
+};
+
+export default GATag;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 import '../utilities/i18n';
 import '../styles/globals.css';
+import GATag from '../components/GATag';
+import FbPixel from '../components/FbPixel';
 
 function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout ? Component.Layout : React.Fragment;
   return (
     <RecoilRoot>
+      <Head>
+        <GATag />
+        <FbPixel />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
